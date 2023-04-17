2228ddcfdf3a89f82b00ed53c9910fc638331354
TaH4ukHero <kosov.misha2016@yandex.ru>
2023-04-18 00:05:15 +0300

    Навод красоты

--- main.py
+++ main.py
@@ -19,14 +19,6 @@ LAUNCH_DIALOG, LETTER_OR_TOWN, LETTER, TOWN = range(4)
 
 towns = [i.strip("\n").replace("ё", "е").lower() for i in open('cities.txt', encoding='utf8')]
 
-
-# towns = ['Ханты Мансийск']
-
-
-# def standart(toponym):
-#     return list(''.join(toponym).replace('ё', 'е').lower())
-
-
 def fix_results(update: Update, context, result):
     sess = create_session()
     user = sess.query(User).filter(User.user_id == update.effective_user.id).first()
@@ -85,42 +77,6 @@ async def launch(update: Update, context):
         return ConversationHandler.END
     await update.message.reply_text('Я тебя не понял. Повтори пожалуйста')
     return LAUNCH_DIALOG
-
-
-# def print_guessed_letters(context):
-#     letters = context.user_data["guessed_letters"]
-#     guessed_town = standart(context.user_data["guessed_town"])
-#     out = []
-#     for val in guessed_town:
-#         if val in letters:
-#             out += guessed_town[guessed_town.index(val)]
-#             guessed_town = list(''.join(guessed_town).replace(val, '*', 1))
-#         elif val == ' ':
-#             out += ' '
-#         else:
-#             out += '_'
-#     return out
-
-
-# def few_facts_abt_town(town):
-#     out = ''
-#     params = {
-#         "geocode": town,
-#         "apikey": APIKEY_GEO,
-#         "format": "json",
-#         "lang": "ru_RU"
-#     }
-#     r = requests.get(url=URL_GEO, params=params).json()
-#     out += f'Название - <b>{town.capitalize()}</b>\n'
-#     with open('town2population.json', encoding='utf8') as file:
-#         population = json.load(file).get(town.capitalize())
-#         out += f"Население - <b>{population}</b>\n"
-#     adm_area_name = r["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-#     adm_area_name = adm_area_name["metaDataProperty"]["GeocoderMetaData"]["AddressDetails"]
-#     adm_area_name = adm_area_name["Country"]["AdministrativeArea"]["AdministrativeAreaName"]
-#     out += f'Административный округ - <b>{adm_area_name}</b>'
-#     return out
-
 
 async def letter_or_town(update: Update, context):
     msg = update.message.text
